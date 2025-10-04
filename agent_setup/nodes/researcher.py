103cd5774777f0c9d4514ff87f012052f64abd24
mohnishmsu962000 <mohnishmsu962000@gmail.com>
2025-10-04 22:56:53 +0530

    Implement researcher node with ChromaDB retrieval and Cohere reranking

--- agent_setup/nodes/researcher.py
+++ agent_setup/nodes/researcher.py
@@ -1,5 +1,20 @@
 from models.schemas import State
+from rag.retriever import retrieve
 
-def researcher_node(state: State) -> State:
-    # TODO: implement researcher logic
-    return state
+def researcher_node(state: State, *, store) -> State:
+    
+    company_data = store.get(("company", "test_company_123"), "metadata")
+    company_id = company_data.value["id"]
+    
+    questions = state['questions']
+    
+    
+    for question in questions:
+        
+        retrieved_chunks = retrieve(question['question_content'], company_id, 5)
+        question['chunks'] = retrieved_chunks
+        
+        
+    return {
+            'questions': questions
+            }
