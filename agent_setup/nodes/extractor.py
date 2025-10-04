e28092dd5b4cb5e12b8122405d83a19c44a60f06
mohnishmsu962000 <mohnishmsu962000@gmail.com>
2025-10-04 20:32:04 +0530

    Implement extractor node with regex pattern matching

--- agent_setup/nodes/extractor.py
+++ agent_setup/nodes/extractor.py
@@ -1,5 +1,23 @@
 from models.schemas import State
+import re
 
 def extractor_node(state: State) -> State:
-    # TODO: implement extraction logic
-    return state
+    questions = []
+    pattern = r'^\s*(\d+)[\.\)]\s*(.+?)$'
+    matches = re.findall(pattern, state['upload_rfp_extracted_text'], re.MULTILINE)
+    
+    question_texts = [match[1].strip() for match in matches]
+    
+    for question_text in question_texts:
+        question = {
+            'question_content': question_text,
+            'question_id': str(len(questions) + 1) ,
+            'chunks': [],
+            'answer': "",
+            'trust_score': 0.0
+        }
+        questions.append(question)
+    
+    return {
+            'questions': questions
+            }
